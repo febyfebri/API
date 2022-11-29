7710618844603860b566ee5c3f3125dd4fe967c8
febyfebri <febrifeby468@gmail.com>
2022-11-30 01:47:18 +0700

    Nambah

---
+++ db_nyepedda.sql
@@ -0,0 +1,41 @@
+/*
+SQLyog Community v13.1.9 (64 bit)
+MySQL - 10.4.27-MariaDB : Database - nyepeda_db
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`nyepeda_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+
+USE `nyepeda_db`;
+
+/*Table structure for table `account_nyepeda` */
+
+DROP TABLE IF EXISTS `account_nyepeda`;
+
+CREATE TABLE `account_nyepeda` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(30) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `kata_sandi` varchar(64) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `account_nyepeda` */
+
+insert  into `account_nyepeda`(`user_id`,`nama`,`email`,`kata_sandi`) values 
+(2,'kimit','kimit1208@gmail.com','tlogowero01'),
+(3,'febykimit','febykimit12@gmail.com','tlogowero04'),
+(6,'sinta','sintawatikusuma@gmail.com','sintawati');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
