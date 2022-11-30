e6cc118733d536fcd9758b61d05ab92659536663
febyfebri <febrifeby468@gmail.com>
2022-11-30 19:19:04 +0700

    nyepeda

--- db_nyepedda.sql
+++ db_nyepedda.sql
@@ -12,9 +12,9 @@ MySQL - 10.4.27-MariaDB : Database - nyepeda_db
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`nyepeda_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_nyepedda` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
 
-USE `nyepeda_db`;
+USE `db_nyepedda`;
 
 /*Table structure for table `account_nyepeda` */
 
